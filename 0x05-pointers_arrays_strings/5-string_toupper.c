d6e3c3593d7b1055408f859d096260b8d0b1a8be
szinte55 <szinte55@gmail.com>
2019-02-23 03:02:42 +0000

    "commit"

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - function that changes lowercase letters to uppercase
+ * in a string
+ *
+ * @c: given string
+ *
+ * Return: pointer to string
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != 0; i++)
+		if ( c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+
+	return (c);
+}
