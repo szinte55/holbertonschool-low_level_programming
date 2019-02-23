d7d68332347463d6377b6880cecac485811d9e93
szinte55 <szinte55@gmail.com>
2019-02-23 00:34:47 +0000

    "commit"

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -14,8 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; src[i] != 0 && i < n; i++)
 		dest[i] = src[i];
 
-	if (i < n)
+	while(i < n)
+	{
 		dest[i] = 0;
+		i++;
+	}
 
 	return (dest);
 }
