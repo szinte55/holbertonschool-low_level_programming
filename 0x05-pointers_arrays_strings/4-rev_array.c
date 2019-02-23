dd0da6a295a4ea1140fccd57516ba6af0aaeb586
szinte55 <szinte55@gmail.com>
2019-02-23 02:02:46 +0000

    "commit"

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0, swap;
+	int i, swap;
 
 	for (i = 0; i < n - 1; i++)
 	{
+		n--;
 		swap = a[n];
 		a[n] = a[i];
 		a[i] = swap;
-		n--;
 	}
 }
