d623dcf2742ba276cb3c9ba506ea6f0d6a1fd5ea
szinte55 <szinte55@gmail.com>
2019-02-23 01:45:58 +0000

    "commit"

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,17 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: return the lexicographic difference of the two strings
+ * Return: return the lexicographic difference of the two passed strings
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int diff = s1[i]-s2[i];
 
-	while( s1[i] != 0 && s2[i] != 0)
+	for (i = 0;  s1[i] != 0 && s2[i] != 0; i++)
 	{
-		if (diff != 0)
+		if (s1[i] - s2[i] != 0)
 			break;
-		i++;
 	}
 
-	return (diff);
+	return (s1[i] - s2[i]);
 }
