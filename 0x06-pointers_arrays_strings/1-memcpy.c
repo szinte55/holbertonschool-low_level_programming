a2b291a4546d5fabd9380ffb0a9e7035eaa5585f
szinte55 <szinte55@gmail.com>
2019-02-25 18:55:26 +0000

    "commit"

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @src: pointer to source string
+ * @n: number of bytes function will copy
+ * @dest: pointer to destination string
+ *
+ * Return: pointer to string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		dest[i] = src[i];
+
+	return (dest);
+
+}
