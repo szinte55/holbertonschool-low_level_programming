2e1101416878d0ef1ee9c0be1d0c2629a021701f
szinte55 <szinte55@gmail.com>
2019-02-28 05:02:10 +0000

    "commit"

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ *
+ * @s: pointer to string for input
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != 0 )
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+}
---
+++ 0x07-recursion/README.md
@@ -0,0 +1,1 @@
+In this project we will be exploring  Recursion.
