f2320a3983190cc52041a267b8a94f0d024059d5
szinte55 <szinte55@gmail.com>
2019-02-28 23:56:02 +0000

    "commit"

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - function that returns the value of x to the power of y
+ * @x: base of exponent
+ * @y: power
+ * Return: the value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return( -1);
+	else if (y > 0)
+		return( x * _pow_recursion( x, y-1));
+	else
+		return (1);
+}
