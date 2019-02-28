437b762e89c2e704e015e2ed8a11d36aaa4aef7d
szinte55 <szinte55@gmail.com>
2019-02-28 18:04:13 +0000

    "commit"

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: pointer to string
+ *
+ * Return: Void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+		return;
+
+	_print_rev_recursion(s+1);
+	_putchar(*s);
+
+}
