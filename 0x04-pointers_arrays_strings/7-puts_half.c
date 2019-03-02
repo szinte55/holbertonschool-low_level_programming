ad670eb34b44a0fff128a291c83941d7ce00d5fb
szinte55 <szinte55@gmail.com>
2019-03-02 23:09:16 +0000

    "commit"

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -11,15 +11,10 @@ void puts_half(char *str)
 
 	for (x = 0; str[x] != 0; x++)
 		;
-	if (x % 2 != 0)
-	{
-		for (n = (x + 1) / 2; n < x; n++)
-			_putchar(str[n]);
-	}
-	else
-	{
-		for (n = x / 2; n < x; n++)
-			_putchar(str[n]);
-	}
+	x--;
+
+	for (n = x / 2 + 1; str[n] != 0; n++)
+		_putchar(str[n]);
+
 	_putchar('\n');
 }
