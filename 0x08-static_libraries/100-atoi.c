e9707721360c798b794f756b7dc8e440ff36a077
szinte55 <szinte55@gmail.com>
2019-03-03 02:37:52 +0000

    "commit"

--- 0x08-static_libraries/100-atoi.c
+++ 0x08-static_libraries/100-atoi.c
@@ -1,3 +1,13 @@
 #include "holberton.h"
+/**
+ * _atoi - a function that is blank for now
+ * @s: point to string
+ *
+ * Return: pointer of destination
+ */
+int _atoi(char *s)
 
-int _atoi(char *s);
+{
+	s--;
+	return (0);
+}
