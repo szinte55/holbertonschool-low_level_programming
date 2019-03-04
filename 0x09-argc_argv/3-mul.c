26a20932305f67037effc18fd48dfd6a3b45ac7d
szinte55 <szinte55@gmail.com>
2019-03-04 22:46:55 +0000

    "commit"

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the product of two integers
+ * passed to the command line
+ * @argc: integer count of arguments
+ * @argv: array of pointers to strings
+ * Return: 0 for multiplication success and 1 otherwise
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+	}
+	return(0);
+}
