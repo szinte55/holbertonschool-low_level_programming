674a90f62e1abb01916eb88c3ddab84af19ca547
szinte55 <szinte55@gmail.com>
2019-03-06 23:32:55 +0000

    "commit"

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	p = malloc(size * sizeof(char));
 	if (p == NULL)
 		return ('\0');
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 		p[i] = c;
 	return (p);
 }
