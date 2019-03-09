c19ae48231d07ec7933f1327acae3493bd8cc40e
szinte55 <szinte55@gmail.com>
2019-03-09 00:58:23 +0000

    "commit"

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
