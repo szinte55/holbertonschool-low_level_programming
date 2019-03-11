60c68a539bbe52c8b8caabb38e5721c7d9fa7f8c
szinte55 <szinte55@gmail.com>
2019-03-11 18:01:43 +0000

    "commit"

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file it was compiled from
+ * Return: 0 for success
+ */
+int main(void)
+{
+	puts(__FILE__);
+
+	return (0);
+}
