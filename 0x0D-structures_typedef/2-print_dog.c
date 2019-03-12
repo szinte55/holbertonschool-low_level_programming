a271504669aceed33b033151e0b22d012a17dab4
szinte55 <szinte55@gmail.com>
2019-03-12 23:37:51 +0000

    "commit"

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,22 +10,16 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-
 	else
 		printf("Name: %s\n", d->name);
-
-	if (d->age == NULL)
-		printf("Age: 0\n");
-
+	if (d->age == 0)
+		printf("Age: %f\n", 0.000000);
 	else
 		printf("Age: %f\n", d->age);
-
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-
 	else
-		printf("Owner: %s\n", d->owner);}
+		printf("Owner: %s\n", d->owner);
 }
