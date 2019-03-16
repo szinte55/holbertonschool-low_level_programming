5beedd6d9ded0f65d829f3590cddffb3ff1ba0e6
szinte55 <szinte55@gmail.com>
2019-03-16 00:00:04 +0000

    "commit"

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
  * print_name -  function that prints a name
  * @name: pointer to char used for name
  * @f: pointer to function
  * Return: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (f == NULL)
