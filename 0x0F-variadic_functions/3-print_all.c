83c1cd4fd8c0d969d0311864173f1e1fe8192892
szinte55 <szinte55@gmail.com>
2019-03-19 16:44:59 +0000

    "commit"

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,17 +1,94 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
+ * print_char - function that prints a char
+ * @args: argument list
+ * Return: void
+ */
+void charprint(va_list args)
+{
+        printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - function that prints an int
+ * @args: argument list
+ * Return: void
+*/
+void intprint(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+
+/**
+ * print_string - function that prints a string
+ * @args: argument list
+ *
+ * Return: void
+ */
+void stringprint(va_list args)
+{
+	char *z = va_arg(args, char *);
+
+	if (!z)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", z);
+}
+/**
+ * print_float - function that prints floats
+ * @args: argument list
+ *
+ * Return: void
+ */
+void floatprint(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
  * print_all - function that prints anything
  * @format: format is a list of types of arguments passed to the function
+ *
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
+	pips1 pips[] = {
+		{'c', charprint},
+		{'i', intprint},
+		{'f', floatprint},
+		{'s', stringprint},
+		{'\0', NULL}
+	};
+	char *seper1 = "", *seper2 = ", ";
 	va_list args;
-	int i = 0;
-	char *temp, *seperator;
+	int i = 0, j = 0;
+
 
 	va_start(args, format);
 
+	while (format !=  NULL && format[i] != 0)
+	{
+		j = 0;
+		while (pips[j].p != 0)
+		{
+			if (format[i] == pips[j].p)
+			{
+				printf("%s", seper1);
+				pips[j].f(args);
+				seper1 = seper2;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
