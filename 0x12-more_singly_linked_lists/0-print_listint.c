2c2dba93e6fab2f214b6030f6bcc58c153fd75bb
szinte55 <szinte55@gmail.com>
2019-03-25 18:04:46 +0000

    "commit"

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list
+ * @h: listint_t argument
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+  size_t count = 0;
+
+  while (h != NULL)
+    {
+      printf("%d\n", h->n);
+      count++;
+      h = h->next;
+    }
+
+
+  return (count);
+}
